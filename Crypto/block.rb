ef734a702d47cf34dae031294659e7b659fba7fc
kuroskira <pat.halo@hotmail.it>
2018-02-26 20:45:51 +0100

    Commented Block.rb

--- Crypto/block.rb
+++ Crypto/block.rb
@@ -4,13 +4,17 @@ require_relative 'pki'
 
 class Block
   NUM_ZEROES = 4 # Difficulty
+
+  # Set the Block properties
   attr_reader :own_hash, :prev_block_hash, :txn
 
+  # Uses key pair to generate the first Block.
   def self.create_genesis_block(pub_key, priv_key)
     genesis_txn = Transaction.new(nil, pub_key, 500_000, priv_key)
     Block.new(nil, genesis_txn)
   end
   
+  # Initialize Block. If it's genesis block we don't have previous hash
   def initialize(prev_block, txn)
     raise TypeError unless txn.is_a?(Transaction)
     @txn = txn
@@ -18,15 +22,18 @@ class Block
     mine_block!
   end
 
+  # Computes the hash of the Block and assigns it.
   def mine_block!
     @nonce = calc_nonce
     @own_hash = hash(full_block(@nonce))
   end
 
+  # Assert that the Transaction in well signed.
   def valid?
     is_valid_nonce?(@nonce) && @txn.is_valid_signature?
   end
 
+  # Pretty printer.
   def to_s
     [
       "Previous hash: ".rjust(15) + @prev_block_hash.to_s.yellow,
@@ -39,10 +46,13 @@ class Block
 
   private
 
+  # Gives back hash256 bit.
   def hash(contents)
     Digest::SHA256.hexdigest(contents)
   end
 
+  # Exaustevely hashes the nonce until it matches the number of
+  # zeroes required by the block to be mined.
   def calc_nonce
     nonce = "HELP I'M TRAPPED IN A NONCE FACTORY"
     count = 0
@@ -54,12 +64,16 @@ class Block
     nonce
   end
 
+  # Asserts that the given nonce hashed with the txn and the
+  # previous hash block starts with the required number of
+  # zeroes.
   def is_valid_nonce?(nonce)
     hash(full_block(nonce)).start_with?("0" * NUM_ZEROES)
   end
 
+  # Returns the string composed by txn, prev_block_hash, nonce
+  # removing eventual nils.
   def full_block(nonce)
     [@txn.to_s, @prev_block_hash, nonce].compact.join
   end
 end
-
